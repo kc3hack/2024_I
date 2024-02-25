495ba6df0c8061cd9ac07d31861694a75344d5be
jimicho <ryoga068068@gmail.com>
2024-02-25 10:25:43 +0900

    アクションバーの追加
    RankingからCamera画面の遷移

--- app/src/main/java/com/example/takoyaki/RankingActivity.java
+++ app/src/main/java/com/example/takoyaki/RankingActivity.java
@@ -93,4 +93,26 @@ public class RankingActivity extends AppCompatActivity {
         //アクションバーに「戻るボタン」を追加
         Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuButton) {
+        //戻り値の変数を定義。trueを初期値として代入
+        boolean result = true;
+        //選択されたメニューボタンのIDを取得(今回は戻るボタンのみのため条件分岐不要だが、追加したときを想定し記載)
+        int buttonId = menuButton.getItemId();
+        //IDで条件分岐(戻るボタンが押されたときの命令以外省略可)
+        switch (buttonId) {
+            //戻るボタンが押されたとき
+            case android.R.id.home:
+                //画面を終了させる
+                finish();
+                break;
+            //それ以外の時
+            default:
+                result = super.onOptionsItemSelected(menuButton);
+                break;
+        }
+        return result;
+    }
+
 }
